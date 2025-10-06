9c8738dc51df066b6ac317f6b5c2183e56ef17f5
SamuelMoscoso-ITB2425 <samuel.moscoso.7e8@itb.cat>
2025-10-06 15:22:49 +0200

    db

--- app/db.php
+++ app/db.php
@@ -1,1 +1,12 @@
-l
+<?php
+$servername = "locahost";
+$username = "root";
+$password = "root";
+$dbname = "crud_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connexió fallida: " . $conn->connect_error);
+}
+?>
